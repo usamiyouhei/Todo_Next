6f29ccd75774b7ce71387afd6645f3aa73445a33
youhei usami <usao9110@icloud.com>
2025-08-13 01:46:20 +0400

    8/13

--- src/features/todo/hooks/useTodos.ts
+++ src/features/todo/hooks/useTodos.ts
@@ -1,6 +1,6 @@
 'useClient'
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { safeLocalStorage } from "@/lib/storage";
 import { Todo } from "@/types/Todo";
 
@@ -9,16 +9,22 @@ const KEY = 'todos:v1';
 export const useTodos = () => {
   const [todos, setTodos] = useState<Todo[]>([])
 
-  const addTodo = ( text: string) => {
-    const newTodo: Todo = {
-      id: Date.now().toString(),
-      text,
-      isDone: false
-    }
-    setTodos([newTodo, ...todos])
+  useEffect(() => { setTodos(safeLocalStorage.get<Todo[]>(KEY, [])) }, [])
+  useEffect(() => {safeLocalStorage.set(KEY, todos)} , [todos])
+
+  
+
+  const add = ( text: string) => {
+    setTodos(prev => [{id: String(Date.now()), text, isDone: false}, ...prev])
   }
 
-  const toggleDone = (id: string) => {
-    setTodos(todos.map(todo => todo.id === id ? {...todo, isDone: !todo.isDone} : todo ) )
+  const toggle = (id: string) => {
+    setTodos(prev => prev.map(t => t.id === id ? {...t, isDone: !t.isDone} : t ) )
   }
+
+  const remove = (id: string) => {
+    setTodos(prev => prev.filter(t => t.id !== id ))
+  }
+
+  return { todos, add, toggle, remove}
 }
